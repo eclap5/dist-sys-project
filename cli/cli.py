75e749da3ffcec1450eb7a30512ba8fffc1c2ea7
emilh_lut <emil.haapalainen@student.lut.fi>
2024-04-23 19:01:18 +0300

    match case and fixes

--- cli/cli.py
+++ cli/cli.py
@@ -5,6 +5,7 @@ from datetime import datetime
 REQ_ENDPOINT = "http://localhost:5050/api/reservations"
 
 def validateTime(date, time):
+    #Parsing datetime string to a datetime object and checking its validity. 
     try:
         datetime_object = datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M")
         return datetime_object.strftime("%Y-%m-%dT%H:%M:%S")
@@ -22,7 +23,7 @@ def makeReservation():
     startDateTime = validateTime(date, startTime)
     endDateTime = validateTime(date, endTime)
 
-    if not startDateTime or not endDateTime: # Ensuring no bad data is sent
+    if not startDateTime or not endDateTime: # Ensuring no bad or nonexisting data is sent
         return
     
     data = {
@@ -33,23 +34,24 @@ def makeReservation():
         "endDateTime": endDateTime
     }
 
+    # dict to json object
     jsonData = json.dumps(data)
 
     response = requests.post(REQ_ENDPOINT, json=jsonData)
-    if response.status_code == 200:
-        print("Reservation created.")
-    else:
+    if response.status_code != 200:
         print(f"Reservation failed: {response.text}")
+    else:
+        print("Reservation created.")
 
 
 def listReservations():
     response = requests.get(REQ_ENDPOINT)
-    if response.status_code == 200:
+    if response.status_code != 200:
+        print(f"Failed to list reservations: {response.text}")
+    else:
         reservations = response.json()
         for reservation in reservations:
             print(reservation)
-    else:
-        print(f"Failed to list reservations: {response.text}")
 
 
 def menu():
@@ -60,15 +62,15 @@ def menu():
 
         choice = input("Enter choice: ")
 
-        if choice == "1":
-            makeReservation()
-        elif choice == "2":
-            listReservations()
-        elif choice == "3":
-            break
-        else:
-            print("Invalid input. Try 1-3.")
-
+        match choice:
+            case "1":
+                makeReservation()
+            case "2":
+                listReservations()
+            case "3":
+                break
+            case _:
+                print("Invalid input. Try 1-3.")
 
 if __name__ == "__main__":
     menu()
