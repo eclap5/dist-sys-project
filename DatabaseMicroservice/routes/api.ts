171f8906cf5a6155a97502e9724cb09086257441
AleksiLe <aleksi.levanen@gmail.com>
2024-04-24 17:31:47 +0300

    Edited get function

--- DatabaseMicroservice/routes/api.ts
+++ DatabaseMicroservice/routes/api.ts
@@ -32,7 +32,18 @@ router.post("/reservations", async (req: Request, res: Response) => {
 router.get("/reservations", async (req: Request, res: Response) => {
   try {
     await mongoose.connect(mongoDB);
-    const reservations: IReservation[] = await Reservation.find();
+    if (!req.body.startDateTime || !req.body.endDateTime) {
+      return res
+        .status(400)
+        .send({ status: "error", message: "Missing fields" });
+    }
+    const reservations: IReservation[] = await Reservation.find(
+      {
+        startDateTime: { $gte: req.body.startDateTime },
+        endDateTime: { $lte: req.body.endDateTime },
+      },
+      { _id: 0, __v: 0 }
+    );
     mongoose.connection.close();
     res.send({ status: "success", reservations: reservations });
   } catch (error) {
